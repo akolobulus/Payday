c1f4a26fae9a13f18564e08bc54cb399b74da546
Akolo Bulus <officialarikpa@gmail.com>
2026-09-13 13:19:21 +0100

    J

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,4 +1,4 @@
-// Comprehensive skill categors for the gig marketplace
+// Comprehensive skcategors for the gig marketplace
 export const SKILL_CATEGORIES = {
   "Art & Creative": [
     "Graphic Design",
